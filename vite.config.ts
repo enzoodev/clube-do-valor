482b0c2a7578051a793b65867a32f82193e08c14
Enzo <ninodm.dev@gmail.com>
2024-02-28 11:01:05 -0300

    Revert "feat: eslint and prettier config"
    This reverts commit b8511629837659fc73ba53ecb9c67303c8cba23d.

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@assets": "/src/assets",
-      "@components": "/src/components",
-      "@dtos": "/src/dtos",
-      "@utils": "/src/utils",
-      "@pages": "/src/pages",
-    },
-  },
-});
+})
