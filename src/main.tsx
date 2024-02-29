31f12d909a37d01d7729b675e206311e131abb39
Enzo <ninodm.dev@gmail.com>
2024-02-29 05:24:15 -0300

    feat: link react-router-dom and global css

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
 
 import { Dashboard } from '@pages/Dashboard.tsx';
+import { store } from '@store/index';
+
+import 'react-toastify/dist/ReactToastify.css';
+import './index.css';
 
 const router = createBrowserRouter([
   {
@@ -13,6 +19,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </Provider>
   </React.StrictMode>,
 );
