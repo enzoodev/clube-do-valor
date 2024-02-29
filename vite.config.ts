188045bfa5b35b852b3a470deb4eea58c6ee1dfc
Enzo <ninodm.dev@gmail.com>
2024-02-29 05:22:46 -0300

    feat: path router config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@assets": "/src/assets",
-      "@components": "/src/components",
-      "@dtos": "/src/dtos",
-      "@utils": "/src/utils",
-      "@pages": "/src/pages",
+      '@assets': '/src/assets',
+      '@components': '/src/components',
+      '@services': '/src/services',
+      '@utils': '/src/utils',
+      '@pages': '/src/pages',
+      '@store': '/src/store',
+      '@hooks': '/src/hooks',
+      '@features': '/src/features',
     },
   },
 });
